00516d42c3267adc4a9c422de026d647832093c2
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-04-14 22:50:03 -0500

    Configuring CD for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig( () => {
-  if (process.env.BUILD_TYPE === 'storybook') {
+  if (process.env.BUILD_COMBINED === 'true' && process.env.BUILD_TYPE === 'storybook') {
     return {
       plugins: [react(),  reactRefresh()],
       base: "/storybook",
