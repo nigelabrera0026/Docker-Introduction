c9678d637911304d2c74cbcde6c07a31159e9fe8
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-04-14 23:26:50 -0500

    Configured vite.config.ts for proper implementation of CD

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,20 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig( () => {
-  if (process.env.BUILD_COMBINED === 'true' && process.env.BUILD_TYPE === 'storybook') {
+export default defineConfig( ({}) => {
+  const config = {
+    plugins: [react(), reactRefresh()],
+    base: process.env.BUILD_PATH_PREFIX || undefined
+  }
+
+  if (process.env.BUILD_COMBINED === "true" && process.env.BUILD_TYPE === "storybook") {
     return {
-      plugins: [react(),  reactRefresh()],
-      base: "/storybook",
-      esbuild: {
-        jsxFactory: "React.createElement",
-        jsxFragment: "React.Fragment",
-      }
+      ...config,
+      base: config.base ? config.base + "/storybook" : "/storybook"
     }
-  } else {
-    return {
-      plugins: [react(),  reactRefresh()],
-      esbuild: {
-        jsxFactory: "React.createElement",
-        jsxFragment: "React.Fragment",
-      }
-    }
+  }
+
+  return {
+    ...config
   }
 })
