9cb7311316c53ce0e18da4f51fdd8cb374f5fa66
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-09-10 19:41:07 -0500

    Modified styling and instructions

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,7 @@ import './App.css'
 import { Button, ButtonProps } from './stories/components';
 import { NavigationBar, NavigationBarProps } from './stories/components/page_components/NavigationBar';
 import { Container, ContainerProps } from './stories/components/Container';
-/* 
-TODO: Figure out what image,
-TODO: Figure out logic flow of the website
-TODO: Configure multi page (?)
-TODO: Modify components and stories
-TODO: Edit figma design and finalize for v0.1;
-*/
+
 /*
  * Logic flow
 TODO: Create section components to build up the pages
@@ -20,9 +14,16 @@ TODO: Check this site for reference:
  *  https://webflow.com/ix2
 */
 
+
+/** Master Plan
+ * FIXME: Configure styling of the nav bar especially the dropdown
+ * TODO: Check if you could separate the props from the main function.
+ * 
+ */
+
 /**
- * The app react component that would be forwarded to the main.
- * @returns Home page
+ * The app react component that would hold all the components to be presented.
+ * @returns Homepage
  */
 function App() {
 
